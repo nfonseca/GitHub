a84c1bf76cdb2f1ff4ef10406ee9d4c2cfad4e61
Nelson Fonseca <nelson.chamusca.fonseca@gmail.com>
2018-02-01 14:29:00 +0000

    Commit

--- net-dump-collector.py
+++ net-dump-collector.py
@@ -1,4 +1,9 @@
 #!/bin/python
 
 #   script that collects network dumps when a condition is met from the logs
+#   functions that the script has to perform:
+#   1 - take continuous network dumps from a vmnic to a file
+#   2 - rotate that file and remove it after a while
+#   3 - continuosuly monitor and parse a log file and search for a string
+#   4 - if the string is found, then stop the captures and send a message
 
